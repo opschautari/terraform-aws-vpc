5037b6803ade41909980084084965e71ddd23088
thapakazi <milans.thapa78@gmail.com>
2020-05-19 15:34:23 +0545

    readme - add readme sample

--- public_route_table.tf
+++ public_route_table.tf
@@ -15,3 +15,11 @@ resource "aws_route" "internet_access" {
   destination_cidr_block = "0.0.0.0/0"
   gateway_id             = aws_internet_gateway.this.id
 }
+
+# associate subnet to public route table
+resource "aws_route_table_association" "subnets_public_association" {
+  count = length(var.public_subnets) > 0 ? length(var.public_subnets) : 0
+
+  subnet_id      = element(aws_subnet.public.*.id, count.index)
+  route_table_id = aws_route_table.public_route_table.id
+}
